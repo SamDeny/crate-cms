02ea9e2128686b247763f7331e1cdc7383c9a641
Sam <sam@rat.md>
2022-06-12 19:35:54 +0200

    [TASK] Still WiP

---
+++ ROADMAP.md
@@ -0,0 +1,13 @@
+Crate CMS - Roadmap
+===================
+
+This roadmap does not directly belong to Crate CMS itself, moreover it contains all planned or under 
+consideration cjanges and features to all core modules including the Citrus Application Framework.
+This file may be removed in future releases, when the roadmap found a neat place on the official
+website: https://crate.md.
+
+
+Version 0.2.0 - Alpha
+=====================
+
+-   Visual Setup / Upgrade Wizard (provided by @crate/core)
--- crate/bootstrap.php
+++ crate/bootstrap.php
@@ -36,48 +36,7 @@ $citrus->setPaths($citrus->getConfiguration('paths'));
 
 
 /**
- * Set Basic Factories
- * ----------------------------------------
- */
-$citrus->setFactories([
-    \Crate\Database\Connection::class   => \Crate\Factories\ConnectionFactory::class,
-    \Crate\Mailer\Mailer::class         => \Crate\Factories\MailerFactory::class
-]);
-
-
-/**
- * Set Basic Services
- * ----------------------------------------
- */
-$citrus->setServices([
-    'session'       => \Crate\Services\SessionService::class,
-    'runtime'       => \Crate\Services\RuntimeService::class
-]);
-
-
-/**
- * Set Basic Aliases
- * ----------------------------------------
- */
-$citrus->setAliases([
-    'mailer'        => \Crate\Mailer\Mailer::class,
-    'mail'          => \Crate\Mailer\Mail::class,
-    'modules'       => \Crate\Modules\ModuleRegistry::class
-]);
-
-
-/**
  * Finish Citrus
  * ----------------------------------------
  */
-$citrus->finalize(\Crate\Services\RuntimeService::class);
-
-
-
-citrus(function(\Citrus\Router\Router $router) {
-    $router->get('/', function() {
-        $response = new \Citrus\Http\Response();
-        $response->setJSON(['hello' => 'world']);
-        return $response;
-    });
-});
+$citrus->finalize(\Crate\Core\Services\RuntimeService::class);
